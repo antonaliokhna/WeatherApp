fdd2b69f5f68247dfd107ed6352dc0c60edd497f
Anton Aliokhna <antonaliokhna@gmail.com>
2022-12-08 01:21:10 +0300

    Init commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Anton Aliokhna on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
